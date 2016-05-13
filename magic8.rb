30eadf56fe937a21cbdc65fe5a073ea63a488a93
bwright9 <bwright984@gmail.com>
2016-05-13 17:00:32 -0600

    Update magic8.rb

--- magic8.rb
+++ magic8.rb
@@ -1,32 +1,34 @@
-def greeting
-  puts "Ask the Magic Ball a question or type Quit to end the program"
-  choice = gets.strip.downcase
-  exit 0 if choice == Quit
-  randomize
-  output 
+def question
+    puts "Ask the Magic Ball a question or type Quit to end the program"
+    user_input = gets.strip.downcase
+    if user_input == "Quit"
+      puts "Thanks for playing Magic 8 Ball. Please play again soon."
+      exit
+    end
 end
 
-def randomize
-  random_select.random   
-end
-
-def random_select
-  [
+@answers = [
+    "Yes",
     "It is certain",
     "Without a doubt",
+    "You may rely on it",
     "Most likely",
+    "Very doubtful",
     "Signs point to yes",
     "Ask again later",
     "Don't count on it",
     "Outlook not so good", 
   ]
-end 
 
 def output
-	puts random_select.random
+  puts "Shake, Shake, Shake.....#{@answers.sample}"
 end
 
-greeting
+while true
+  question
+  output
+
+end
 
 
 
