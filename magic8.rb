66b213a27b9f7d06b2b49175105b8a0b416563e7
Brant Wright <bwright984@gmail.com>
2016-05-12 20:30:54 -0600

    first edit

--- magic8.rb
+++ magic8.rb
@@ -1,7 +1,35 @@
-#print a welcome message 
-#get first number
-#get operator
-#validate correct operator
-#get second number
-#print output 
-#prompt to continue or exit 
+def greeting
+  puts "Ask the Magic Ball a question or type Quit to end the program"
+  choice = gets.strip.downcase
+  exit 0 if choice == Quit
+  randomize
+  output 
+end
+
+def randomize
+  random_select.random   
+end
+
+def random_select
+  [
+    "It is certain",
+    "Without a doubt",
+    "Most likely",
+    "Signs point to yes",
+    "Ask again later",
+    "Don't count on it",
+    "Outlook not so good", 
+  ]
+end 
+
+def output
+	puts random_select.random
+end
+
+greeting
+
+
+
+
+
+
